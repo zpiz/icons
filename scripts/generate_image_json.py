2ff0292d5fd3f6a09131da137a3182d6878b5dce
zpiz <134020066+zpiz@users.noreply.github.com>
2026-01-29 00:10:31 +0800

    Update JSON generation for icon names and output file

--- scripts/generate_image_json.py
+++ scripts/generate_image_json.py
@@ -4,7 +4,7 @@ import json
 def generate_json():
     image_folder = 'icon'
     json_data = {
-        "name": "SLinks自用图标",
+        "name": "Links自用图标",
         "description": "Links自用图标",
         "icons": []
     }
@@ -17,7 +17,7 @@ def generate_json():
             json_data["icons"].append(icon_data)
 
     # Set the output path relative to the repository root
-    output_path = os.path.join(os.getcwd(), 'sliverkiss.icons.json')
+    output_path = os.path.join(os.getcwd(), 'links.icons.json')
 
     with open(output_path, 'w', encoding='utf-8') as json_file:
         json.dump(json_data, json_file, ensure_ascii=False, indent=2)
